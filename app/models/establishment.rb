010d80423969388a107c171279fcabea8b6fb33a
Dmg5th <Dmgould310@gmail.com>
2020-10-08 17:29:52 -0400

    Added more associations to models

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,6 +1,6 @@
 class Establishment < ApplicationRecord
     belongs_to :category
-    belongs_to :user
+    belongs_to :user #creator of it 
     has_many :reviews
     has_many :users, through: :reviews 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :establishments, through: :reviews
+    has_many :establishments #that have been created by a user 
 end 
+ 
