84bedc8ee9c875bd489f4a74ab6d3aa2d29d6b62
Dmg5th <Dmgould310@gmail.com>
2020-10-09 09:06:05 -0400

    Added Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+    def new
+      @user = User.new 
+    end 
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/welcome'
+      else 
+        redirect_to new_user_path
+      end
+    end 
+
+private 
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end 
 end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#welcome'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/signup' => 'users#new '
+  get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   resources :reviews
   resources :establishments
