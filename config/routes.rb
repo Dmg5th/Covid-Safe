cfa5b7468f4a4e99849390e301e9e44a73f7c157
Dmg5th <Dmgould310@gmail.com>
2020-10-09 07:44:28 -0400

    added reviewed_establishments to User model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :establishments
   resources :categories
   resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# root to: 'users#new'
+
 end
