946d8b06cf02f5d480d7cf50fb46f2113e103b74
Dmg5th <Dmgould310@gmail.com>
2020-10-12 08:30:12 -0400

    Added validations to review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,5 +8,24 @@ class ReviewsController < ApplicationController
       @review = @establishment.reviews.build
     end 
 
+    def create
+      # byebug
+      @review = Review.new(review_params)
+      if @review.save
+      redirect_to review_path(@review)
+      else 
+        render :new 
+      end 
+    end 
+
+    def show
+    end 
+
+  private
+
+    def review_params
+      params.require(:review).permit(:good_air_ventilation, :staff_practicing_safe_covid_protocols, :seats_far_apart, :outside_seating, :additional_comments, :establishment_id )
+    end 
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :establishment
+    validates :good_air_ventilation, :staff_practicing_safe_covid_protocols, :seats_far_apart, numericality: true
 end
