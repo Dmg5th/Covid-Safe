b65c8bebfb15dddb5a4665a2b32bc8eb0feb69ed
Dmg5th <Dmgould310@gmail.com>
2020-10-12 11:48:16 -0400

    Added more validations for Estblishment

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -3,5 +3,15 @@ class Establishment < ApplicationRecord
     belongs_to :user #creator of it , maybe take out?
     has_many :reviews
     has_many :users, through: :reviews 
-    accepts_nested_attributes_for :category
+    # accepts_nested_attributes_for :category
+    validates :name, presence: true
+   
+
+
+    def category_attributes=(attributes)
+      category = Category.find_or_create_by(attributes) if !attributes[:name].empty?
+    end 
+
+
+
 end
