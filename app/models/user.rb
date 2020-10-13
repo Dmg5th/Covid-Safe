120dcfcdc31459324ba6be0b769ae17aaebff024
Dmg5th <Dmgould310@gmail.com>
2020-10-13 08:11:07 -0400

    git ignore .env file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
     has_many :reviews
     has_many :reviewed_establishments, through: :reviews, source: :establishment #because of bottom relationship
     has_many :establishments #that have been created by a user 
