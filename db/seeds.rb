2862dd87099f8bf1a71d30841298196573f26224
Dmg5th <Dmgould310@gmail.com>
2020-10-14 08:46:04 -0400

    Seeded some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Category.destroy_all
+
+Category.create(name: "Restaurant")
 Category.create(name: "Retail")
 Category.create(name: "Hotel")
 Category.create(name: "Bar")
 Category.create(name: "Coffee Shop/Cafe")
+Category.create(name: "Gym")
+Category.create(name: "Medical")
+
